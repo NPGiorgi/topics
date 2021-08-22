2722480cd911a431e19a804c84c19b7d876912d0
Nicolas Perez <n.perezgiorgi@gmail.com>
2021-08-22 17:29:16 -0300

    Break build

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from "./App";
 
 test("renders learn topics link", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn topics!/i);
+  const linkElement = screen.getByText(/learn react!/i);
   expect(linkElement).toBeInTheDocument();
 });
