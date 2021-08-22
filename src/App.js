d0c6b5a3a303196b38f61d52a162e254c1628cb1
Nicolas Perez <n.perezgiorgi@gmail.com>
2021-08-22 17:25:36 -0300

    Update main header

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Topics
+          Learn Topics!
         </a>
       </header>
     </div>
--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from "./App";
 
 test("renders learn topics link", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn topics/i);
+  const linkElement = screen.getByText(/learn topics!/i);
   expect(linkElement).toBeInTheDocument();
 });
