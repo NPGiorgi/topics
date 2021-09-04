cc3d700a61e7bc1411a84b28954c4da455ece0fb
Nicolas Perez <n.perezgiorgi@gmail.com>
2021-09-04 19:45:06 -0300

    Create .env file with local build

--- platform/build.sh
+++ platform/build.sh
@@ -6,10 +6,13 @@ poetry install
 
 if [[ -z "${CI}" ]]; then
   poetry run black .
+  if ! test -f ".env"; then
+    echo "DB_CONNECTION_URL=postgresql+psycopg2://postgres:cantguessthis@127.0.0.1/platform" >> .env
+    echo "Local .env file created."
+  fi
 else
   poetry run black --check .
 fi
-
 
 poetry run pytest --cov=src --cov-fail-under=96 --no-cov-on-fail --cov-branch tests
 poetry run coverage html --directory reports/coverage/
