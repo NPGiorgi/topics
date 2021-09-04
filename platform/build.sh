90b59de4e236e9a3bea0fa213b35ff1b688bcf1b
Nicolas Perez <n.perezgiorgi@gmail.com>
2021-09-04 19:42:07 -0300

    Add support for database in ci

--- platform/build.sh
+++ platform/build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 poetry install
 
 if [[ -z "${CI}" ]]; then
@@ -7,6 +9,7 @@ if [[ -z "${CI}" ]]; then
 else
   poetry run black --check .
 fi
+
 
 poetry run pytest --cov=src --cov-fail-under=96 --no-cov-on-fail --cov-branch tests
 poetry run coverage html --directory reports/coverage/
