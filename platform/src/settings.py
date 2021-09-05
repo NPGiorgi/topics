c1a94c852eff862a36de12b389fe3185fb60f2be
Nicolas Perez <n.perezgiorgi@gmail.com>
2021-09-05 13:28:32 -0300

    Fix uri for heroku support

--- platform/src/settings.py
+++ platform/src/settings.py
@@ -4,3 +4,6 @@ config = Config("./.env")
 
 DEBUG = config("DEBUG", cast=bool, default=False)
 DATABASE_URL = config("DATABASE_URL", cast=str)
+
+if DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
